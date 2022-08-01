636d464254b77d6c379937cd27b729b4692a0079
rmisu <roger889@umn.edu>
2022-07-31 23:36:51 -0500

    added routes but unable to initialize server due to errors. Believe it is related to downloaded packages somehow

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,12 +7,10 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     id: {
-      type: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-      }
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
     product_id: {
       type: DataTypes.INTEGER,
